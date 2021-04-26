f773d3a410d3cdd546383693a758d3ba8972940b
Walison Fernandes <walisonfbalves.ti@gmail.com>
2021-04-26 10:51:02 -0300

    Praticando

---
+++ 2. Variáveis e Operadores/2.1. Declarando uma Variável/ex001.js
@@ -0,0 +1,13 @@
+var nome = "Walison";
+console.log ("Meu nome é " + nome);
+
+var idade = 26;
+console.log ("Tenho " + idade + " anos")
+
+var valor = 1800.99;
+var desconto = 100;
+var precoFinal = valor - desconto;
+
+console.log ("Quero comprar uma cadeira gamer de R$ " + precoFinal);
+
+
---
+++ 2. Variáveis e Operadores/2.1. Declarando uma Variável/ex002.js
@@ -0,0 +1,10 @@
+var nome = "Walison"
+var nome = "Fernandes"
+
+let apelido = "Wal"
+let apelido = "Walzera" // vai dar erro por causa da variável "apelido" repetida 
+
+
+// Diferença de let para var:
+// var conseguimos redeclarar uma váriavel
+// let não redeclara 
