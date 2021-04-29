b46f41b695d6dd448427f1bd1498a82a12e968d5
Walison Fernandes <walisonfbalves.ti@gmail.com>
2021-04-29 11:39:41 -0300

    Estrutura condicionais
    Dois exercicios

---
+++ 3. Estruturas Condicionais/ex001.js
@@ -0,0 +1,23 @@
+// Introdução:
+
+// if / else; else if; switch.
+
+// Estrutura condicional (if)
+if (idade >= 18) {
+    console.log ('Maior de idade, pode ser preso');
+}
+else {
+    console.log ('Menor de idade, vai tomar seu toddynho');
+}
+
+// Estrutura condicional (switch)
+switch (ddd) {
+    case 11:
+        console.log ("São Paulo");
+        break;
+    case 61:
+        console.log ("Brasília")
+        break;
+}
+
+// Estruturas condicionais são úteis quando precisamos que o sistema toma uma ou mais decisões sem a intervenção do programador.
---
+++ 3. Estruturas Condicionais/ex002.js
@@ -0,0 +1,35 @@
+// if / else
+
+// A estrutura condicional trabalha com estrutura booleanas ou seja true e false.
+
+// Exemplos
+
+if ( limite >= valor) {
+    console.log ("Compra aprovada")
+}
+else {
+    console.log ("Compra negada")
+}
+
+
+if ( reporta != 3) {
+    console.log ('Reposta errada')
+}
+else {
+    console.log ("Resposta certa")
+}
+
+
+if ( idade >= 18 && idade <= 24) {
+    console.log ("Você está na faixa etária entre 18 e 24 anos")
+}
+else {
+    console.log ("Você não está na faixa etária")
+}
+
+if ( reposta == 32) {
+    console.log ("Reposta correta")
+}
+else {
+    console.log ("Reposta errada")
+}
