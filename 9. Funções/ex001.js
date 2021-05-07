0991af1d3fff35f87603a56360cb17e9a040d53d
Walison Fernandes <walisonfbalves.ti@gmail.com>
2021-05-07 10:45:14 -0300

    Update ex001.js

---
+++ 9. Funções/ex001.js
@@ -0,0 +1,17 @@
+// Funções sem parâmetro
+
+// O que é parâmetro?
+
+// Parâmetro é um valor que uma função recebe para usar no seu bloco de código.
+
+// Funções sem parâmetro não vão receber valores para executar em seus blocos de código
+
+// Exemplo de função sem parâmetro
+
+function retorna_versao(); // Nenhum parâmetro passado para a função
+{
+    return 1.1; // Retorno da versão
+}
+
+let versao = retorna_versao(); // atribuimos na variável "versao" a função que nos retorna a versão de uma aplicação
+console.log (versao); // Por fim, imprimimos a variável
